fbe876cebc989dd3ce13ff4d40fd05a887068461
David Kontorovsky <1487550+dkvsky-repo@users.noreply.github.com>
2022-09-04 03:07:28 +0000

    Vite config update, etc

--- backup/vite.config.js
+++ backup/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import svelte from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    host: true
-  },
-  resolve: {
+    plugins: [svelte()],
+    server: {
+        hmr: {
+            port: 443,
+        }
+    },
+    resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/lib/components'),
-    }
-  }
+    },
+  },
 })
